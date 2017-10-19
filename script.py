99e81c4f01e890096bb47a4b011f31444cf689ea
Murali-SMU <knd.muralidhar@gmail.com>
2017-10-19 23:11:34 +0800

    updated file

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import re
 import urllib
 
 #connect to a URL
-course = "gre"
+course = "gmat"
 url = "https://"+course+".magoosh.com/lessons"
 website = urllib2.urlopen(url)
 
@@ -11,20 +11,21 @@ website = urllib2.urlopen(url)
 html = website.read()
 
 #use re.findall to get all the links
-links = re.findall('\/lessons\/.*', html)
+links = re.findall('\/lessons\/.*"><', html)
 #print links;
 for i in links:
-	url = i[:-2];
+	url = i[:-3];
 	name =  url.split("/lessons/")[1];
-	print name;	
 	url =  "https://"+course+".magoosh.com"+url;
-	website = urllib2.urlopen(url)
+	print url;
+	website = urllib.urlopen(url)
+	#print urllib.urlopen(url).read()
+	#print website," website";	
 	html = website.read()
 	#link = re.findall('https:.*.cloudfront.net.audio_video.*-video-.*.web.mp4', html)
-	link = re.findall('<li.* class=.current.*\n<a.*\n<span.*', html)
-	#print link;
+	#link = re.findall('<div.* class=.current.*\n<a.*\n<span.*', html)
+	link = re.findall('<div.*.current".*.div>', html)
 	link = re.findall('https.*.web',link[0])
 	link = link[0]+".mp4";
-	print link;
 	test=urllib.URLopener()
 	test.retrieve(link,name+".mp4");
